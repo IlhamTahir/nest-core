c14daef98115aea32c5e5d2474d53fec13ac2627
IlhamTahir <yantaq@bilig.biz>
2025-08-02 12:36:22 +0800

    feat: update version to 0.1.2 and enhance exports in index.ts

--- packages/nest-core/src/index.ts
+++ packages/nest-core/src/index.ts
@@ -1,6 +1,20 @@
 export * from './constants/jwt'
-
 export * from './decorators'
-
-
 export * from './core.module'
+
+// 导出实体相关
+export * from './entity/base.entity'
+
+// 导出 DTO 相关
+export * from './dto/base.filter'
+
+// 导出 VO 相关
+export * from './vo/base.vo'
+export * from './vo/page-result'
+
+// 导出 Mapper 相关
+export * from './mapper/base.mapper'
+export * from './mapper/page-result.mapper'
+
+// 重新导出 TypeORM 模块
+export { TypeOrmModule } from '@nestjs/typeorm'
